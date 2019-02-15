6b5c76b105ede73fb2e1924719dd433aa17c1a27
Dylan Clements <dclements@protonmail.com>
2019-02-15 19:05:12 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,4 +9,14 @@ attr_accessor :name
   def self.all
     @@all.each {|patient| patient.name}
   end
+  def new_appointment(doctor,date)
+    Appointment.new = (self, doctor, date)
+  end
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.map{|appointment| appointment.doctor}
+  end
 end
